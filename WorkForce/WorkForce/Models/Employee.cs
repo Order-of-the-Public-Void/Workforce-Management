5d0a160fb468deb03a5865bbdccc16c6b41d1312
Andrea Finch <aefinch@vt.edu>
2017-10-17 20:05:21 -0500

    Revert "addEmployee in process"

--- WorkForce/WorkForce/Models/Employee.cs
+++ WorkForce/WorkForce/Models/Employee.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace WorkForce.Models
 {
 	public class Employee
-	{   
-        [Required]
+	{
 		public int EmployeeId {get; set;}
 		public string LastName { get; set; }
 		public string FirstName { get; set; }
-        public DateTime StartDate { get; set; }
-        public int DeptId { get; set; }
+		public int DeptId { get; set; }
 	}
 }
